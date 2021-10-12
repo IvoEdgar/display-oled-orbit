5f18ad998e24d9ab82ddfe198c44878050d2d310
Ivo Edgar <ivo.edgy@gmail.com>
2021-10-12 19:20:52 -0300

    Atualizar 'main.blocks, main.ts'

--- main.ts
+++ main.ts
@@ -1,10 +1,84 @@
+input.onButtonPressed(Button.A, function () {
+    DrawBalls(0)
+})
+input.onButtonPressed(Button.B, function () {
+    DrawBalls(1)
+})
+function DrawBalls (cor: number) {
+    for (let índice = 0; índice <= 2; índice++) {
+        OLED12864_I2C.rect(
+        sunX + sunPos1[índice],
+        sunY + sunPos1[5 - índice],
+        sunX + sunPos2[índice],
+        sunY + sunPos2[5 - índice],
+        cor
+        )
+        OLED12864_I2C.rect(
+        sunX + sunPos1[5 - índice],
+        sunY + sunPos1[índice],
+        sunX + sunPos2[5 - índice],
+        sunY + sunPos2[índice],
+        cor
+        )
+    }
+    OLED12864_I2C.rect(
+    sunX + 6,
+    sunY + 6,
+    sunX + 12,
+    sunY + 12,
+    cor
+    )
+    OLED12864_I2C.rect(
+    sunX + 8,
+    sunY + 8,
+    sunX + 10,
+    sunY + 10,
+    cor
+    )
+    OLED12864_I2C.pixel(sunX + 9, sunY + 9, cor)
+    OLED12864_I2C.rect(
+    planetX + 2,
+    planetY + 2,
+    planetX + 8,
+    planetY + 8,
+    cor
+    )
+    OLED12864_I2C.rect(
+    planetX + 4,
+    planetY + 1,
+    planetX + 6,
+    planetY + 9,
+    cor
+    )
+    OLED12864_I2C.rect(
+    planetX + 1,
+    planetY + 4,
+    planetX + 9,
+    planetY + 6,
+    cor
+    )
+    OLED12864_I2C.rect(
+    planetX + 3,
+    planetY + 3,
+    planetX + 7,
+    planetY + 7,
+    cor
+    )
+    OLED12864_I2C.pixel(planetX + 5, planetY + 5, cor)
+}
+let sunPos2: number[] = []
+let sunPos1: number[] = []
+let sunY = 0
+let sunX = 0
+let planetY = 0
+let planetX = 0
 OLED12864_I2C.init(60)
 OLED12864_I2C.zoom(false)
-let planetX = 84
-let planetY = 26
-let sunX = 54
-let sunY = 22
-let sunPos1 = [
+planetX = 84
+planetY = 26
+sunX = 54
+sunY = 22
+sunPos1 = [
 7,
 5,
 4,
@@ -12,7 +86,7 @@ let sunPos1 = [
 2,
 1
 ]
-let sunPos2 = [
+sunPos2 = [
 11,
 13,
 14,
@@ -89,6 +163,3 @@ planetY + 7,
 )
 basic.pause(2000)
 OLED12864_I2C.pixel(planetX + 5, planetY + 5, 1)
-basic.forever(function () {
-	
-})
