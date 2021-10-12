d126cd087f2e3ce4e737b44e21a50e0fda9792e8
Ivo Edgar <ivo.edgy@gmail.com>
2021-10-12 00:39:36 -0300

    Atualizar 'main.blocks, main.ts'

--- main.ts
+++ main.ts
@@ -19,9 +19,12 @@ let sunSizeArray = [
 17
 ]
 for (let índice = 0; índice <= 2; índice++) {
-    kitronik_VIEW128x64.drawRect(sunSizeArray[índice], sunSizeArray[5 - índice], sunX + sunPosArray[índice], sunY + sunSizeArray[5 - índice])
-    kitronik_VIEW128x64.drawRect(sunSizeArray[5 - índice], sunSizeArray[índice], sunX + sunSizeArray[5 - índice], sunY + sunPosArray[índice])
+    kitronik_VIEW128x64.drawRect(sunSizeArray[índice], sunSizeArray[5 - índice], sunX + sunPosArray[índice], sunY + sunPosArray[5 - índice])
+    kitronik_VIEW128x64.drawRect(sunSizeArray[5 - índice], sunSizeArray[índice], sunX + sunPosArray[5 - índice], sunY + sunPosArray[índice])
 }
+kitronik_VIEW128x64.drawRect(7, 7, sunX + 6, sunY + 6)
+kitronik_VIEW128x64.drawRect(3, 3, sunX + 8, sunY + 10)
+kitronik_VIEW128x64.setPixel(sunX + 9, sunX + 9)
 basic.forever(function () {
 	
 })
