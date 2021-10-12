c3dad481f0d6ae1bdee6ada2b21ca295518bb0b0
Ivo Edgar <ivo.edgy@gmail.com>
2021-10-12 16:27:59 -0300

    Atualizar 'main.blocks, main.ts'

--- main.ts
+++ main.ts
@@ -20,17 +20,25 @@ let sunSizeArray = [
 ]
 for (let índice = 0; índice <= 2; índice++) {
     kitronik_VIEW128x64.drawRect(sunSizeArray[índice], sunSizeArray[5 - índice], sunX + sunPosArray[índice], sunY + sunPosArray[5 - índice])
+    basic.pause(1000)
     kitronik_VIEW128x64.drawRect(sunSizeArray[5 - índice], sunSizeArray[índice], sunX + sunPosArray[5 - índice], sunY + sunPosArray[índice])
+    basic.pause(1000)
 }
 kitronik_VIEW128x64.drawRect(7, 7, sunX + 6, sunY + 6)
+basic.pause(1000)
 kitronik_VIEW128x64.drawRect(3, 3, sunX + 8, sunY + 8)
+basic.pause(1000)
 kitronik_VIEW128x64.setPixel(sunX + 9, sunY + 9)
 basic.pause(1000)
-kitronik_VIEW128x64.drawRect(7, 7, planetX + 2, planetY + 2)
-kitronik_VIEW128x64.drawRect(3, 9, planetX + 4, planetY + 1)
-kitronik_VIEW128x64.drawRect(9, 3, planetX + 1, planetY + 4)
-kitronik_VIEW128x64.drawRect(5, 5, planetX + 3, planetY + 3)
 kitronik_VIEW128x64.setPixel(planetX + 5, planetY + 5)
+basic.pause(1000)
+kitronik_VIEW128x64.drawRect(7, 7, planetX + 2, planetY + 2)
+basic.pause(1000)
+kitronik_VIEW128x64.drawRect(3, 9, planetX + 4, planetY + 1)
+basic.pause(1000)
+kitronik_VIEW128x64.drawRect(9, 3, planetX + 1, planetY + 4)
+basic.pause(1000)
+kitronik_VIEW128x64.drawRect(5, 5, planetX + 3, planetY + 3)
 basic.forever(function () {
 	
 })
